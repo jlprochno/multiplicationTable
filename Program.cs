cdca7cf33e3e57849910c9624bcd53e44358ab9b
Jenyffer Prochno <jlprochno@gmail.com>
2024-02-09 10:03:53 -0300

    Publishing

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace multiplicationTable;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Olá, por gentileza, digite o número para calculo da tabuada: ");
+        int numero = Convert.ToInt32(Console.ReadLine());
+        int contador = 1;
+        
+        while(contador <= 10){
+            int resultado = contador * numero;
+            Console.WriteLine(contador+ "x"+numero+" = "+ resultado);
+            contador++;
+            }
+    }
+}
